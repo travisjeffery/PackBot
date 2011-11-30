2624889a4d2b5ec89ca1ca795d4f0a2560934d42
Travis Jeffery <travisj@nulogy.com>
2011-11-30 05:34:49 +0000

    authenticate with google

---
+++ conf.js
@@ -0,0 +1,6 @@
+module.exports = {
+  google: {
+      clientId: '160461953011.apps.googleusercontent.com'
+    , clientSecret: 'cbQhvPZJpn1HBqc1VwgZEu_l'
+  }
+};
--- server.js
+++ server.js
@@ -1,16 +1,64 @@
-var express = require('express'),
-    faye = require('faye'),
-    mu = require('mu');
+var everyauth = require('everyauth')
+  , conf = require('./conf')
+  , express = require('express')
+  , connect = require('connect')
+  , faye = require('faye')
+  , mu = require('mu')
+  , app = express.createServer();
 
 var bayeux = new faye.NodeAdapter({
   mount: '/faye'
 , timeout: 45
 });
 
-var app = express.createServer();
+everyauth.debug = true; 
+
+var usersById = {};
+var nextUserId = 0;
+var usersByGoogleId = {};
+
+function addUser(source, sourceUser) {
+  var user;
+  if (arguments.length === 1) {
+    user = sourceUser = source;
+    user.id = ++nextUserId;
+    return usersById[nextUserId] = user;
+  } else {
+    user = usersById[++nextUserId] = {id: nextUserId};
+    user[source] = sourceUser;
+  }
+  return user;
+}
+
+everyauth.everymodule
+  .findUserById(function(id, callback){
+    callback(null, usersById[id]);
+  });
+
+everyauth.google
+  .findOrCreateUser(function(session, accessToken, extra, googleUser){
+    googleUser.refreshToken = extra.refreshToken;
+    googleUser.expiresIn = extra.expires_in;
+    return usersByGoogleId[googleUser.id] || (usersByGoogleId[googleUser.id] = addUser('google', googleUser));
+  })
+  .appId(conf.google.clientId)
+  .appSecret(conf.google.clientSecret)
+  .scope('https://www.google.com/m8/feeds/')
+  .entryPath('/auth/google')
+  .redirectPath('/');
+
+var routes = function(app) {
+
+};
+
 app.configure(function() {
+  app.use(connect.logger());
+  app.use(express.cookieParser());
+  app.use(express.session({secret: 'keyboard cat'}))
   app.use(express.bodyParser());
   app.use(express.static(__dirname + '/public'));
+  app.use(everyauth.middleware());
+  app.use(app.router);
 });
 
 app.post('/message', function(req, res) {
@@ -19,5 +67,15 @@ app.post('/message', function(req, res) {
 });
 
 bayeux.attach(app);
-app.listen(8000);
 
+app.configure(function(){
+  app.set('view engine', 'jade');
+});
+
+app.get('/', function(req, res){
+  res.render('home');
+});
+
+everyauth.helpExpress(app);
+
+app.listen(8000);
